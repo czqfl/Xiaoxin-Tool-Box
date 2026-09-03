f3bf9b293d5cc7e01cca1c26a39b84a282764fd0
张旭4 <1808735293@qq.com>
2026-09-03 15:00:34 +0800

    chore: 清理误入库的临时脚本 + 每日日志收尾（LF）

--- .workbuddy/tmp_p4.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-p = r"D:/My-Custom-Tool/Xiaoxin-Tool-Box/src/settings/ScreenshotPage.tsx"
-with open(p, encoding="utf-8", newline="") as f:
-    L = f.read().split("\r\n")
-for i in range(0, 10):
-    print(f"{i+1:3d}| {L[i]}")
